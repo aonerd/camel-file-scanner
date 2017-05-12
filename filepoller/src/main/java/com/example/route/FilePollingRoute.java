eae3a8838291cae1bba3b8b54967408d2d65b946
Aiden O'Neill <aiden.m.oneill@gmail.com>
2017-05-12 23:36:53 +0100

    Test 1

--- filepoller/src/main/java/com/example/route/FilePollingRoute.java
+++ filepoller/src/main/java/com/example/route/FilePollingRoute.java
@@ -17,17 +17,22 @@ public class FilePollingRoute extends RouteBuilder {
     @Value("${input.path}")
     String inputPath;
 
+
+    /**
+     * 
+     * @throws Exception
+     */
     @Override
     public void configure() throws Exception {
         final String path = "file:" + inputPath
                         + "?move=backup/${date:now:yyyyMMddHMs}/${file:name}";
 
         from(path)
-                  // .process(new toMultipart())
-                  .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-                  .setHeader(Exchange.CONTENT_TYPE, constant("multipart/form-data"))
-                  .setHeader("FILE-NAME", simple("${file:name}"))
-                  .to("http://" + host + ":" + port + "/file");
+          // .process(new toMultipart())
+          .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+          .setHeader(Exchange.CONTENT_TYPE, constant("multipart/form-data"))
+          .setHeader("FILE-NAME", simple("${file:name}"))
+          .to("http://" + host + ":" + port + "/file");
 
     }
 }
